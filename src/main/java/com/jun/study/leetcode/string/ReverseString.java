a965bf5101459cd0ec1321d318b897bdf2e07198
jun <2456868764@qq.com>
2023-03-02 08:30:57 +0800

    344: 反转字符串

--- src/main/java/com/jun/study/leetcode/string/ReverseString.java
+++ src/main/java/com/jun/study/leetcode/string/ReverseString.java
@@ -1,11 +1,17 @@
 package com.jun.study.leetcode.string;
 
 /**
- * https://leetcode.cn/problems/reverse-string/
+ * easy: https://leetcode.cn/problems/reverse-string/
  */
 public class ReverseString {
 
-    public void reverse(char[] chs) {
+    public void reverse(char[] s) {
+
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char ch = s[i];
+            s[i] = s[j];
+            s[j] = ch;
+        }
 
     }
 }
